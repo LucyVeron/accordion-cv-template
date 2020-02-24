14b3509f8b3669a7f585f1af15d6c2fc634436c0
Lucy Veron <veronl@TLTGES.local>
2020-02-24 22:54:49 +0100

    Vertical stepper

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+
+  folders: any[] = [
+    {
+      name: 'Software Engineer - telent GmbH',
+      dates: 'June 2018 - present',
+    },
+    {
+      name: 'Junior Software Engineer - diconium GmbH',
+      dates: 'June 2017 - June 2018',
+    },
+    {
+      name: 'Freelance Fullstack Developer',
+      dates: 'Jan 2017 - Mar 2018',
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,9 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatStepperModule } from '@angular/material/stepper';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 const MATERIAL = [
+  MatCardModule,
+  MatButtonModule,
+  MatStepperModule,
+  MatListModule,
+  MatIconModule
 ];
 
 @NgModule({
@@ -12,7 +24,10 @@ const MATERIAL = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     ...MATERIAL
   ],
   providers: [],
