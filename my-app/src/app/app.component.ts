830e076091be365b22c1eeb0e5e751cb2736836b
lucy.veron@telent.de <lucy.veron@telent.de>
2020-02-25 16:15:22 +0100

    Work, internships, education

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,29 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  folders: any[] = [
-    {
-      name: 'Software Engineer - telent GmbH',
-      dates: 'June 2018 - present',
-    },
-    {
-      name: 'Junior Software Engineer - diconium GmbH',
-      dates: 'June 2017 - June 2018',
-    },
-    {
-      name: 'Freelance Fullstack Developer',
-      dates: 'Jan 2017 - Mar 2018',
-    }
-  ];
+export class AppComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 }
